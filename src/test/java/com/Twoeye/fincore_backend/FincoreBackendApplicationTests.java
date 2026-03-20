5c3cf89c06e530a04aa5b994c5103fdf1a89dae9
zero2top <tjdanr0502@gmail.com>
2026-03-20 12:08:17 +0900

    chore: 초기 빌드 완료.

---
+++ src/test/java/com/Twoeye/fincore_backend/FincoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Twoeye.fincore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
