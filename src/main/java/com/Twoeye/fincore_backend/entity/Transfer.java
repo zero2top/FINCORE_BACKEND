09006b133cb6c08e1b836004f0679513b3e430ff
강성훈 <kshevl123@gmail.com>
2026-03-21 12:31:49 +0900

    feat: Transfer Entity class 추가

---
+++ src/main/java/com/Twoeye/fincore_backend/entity/Transfer.java
@@ -0,0 +1,46 @@
+package com.Twoeye.fincore_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Transfers")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long fromAccountId;
+
+    @Column(nullable = false)
+    private Long toAccountId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+//    @Column(nullable = false)
+//    private enum status;
+
+    @Column
+    private String failReason;
+
+    @Column(unique = true,nullable = false)
+    private String idempotencyKey;
+
+    @Column(nullable = false)
+    private LocalDate requestedTime;
+
+    @Column(nullable = false)
+    private LocalDate completedTime;
+
+}
