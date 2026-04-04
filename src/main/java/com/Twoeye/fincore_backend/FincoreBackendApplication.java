3f30a1dadef2779da82904aee723df2bf3705386
강성훈 <kshevl123@gmail.com>
2026-04-04 11:48:45 +0900

    chore: spring-security 추가

--- src/main/java/com/Twoeye/fincore_backend/FincoreBackendApplication.java
+++ src/main/java/com/Twoeye/fincore_backend/FincoreBackendApplication.java
@@ -2,8 +2,10 @@ package com.Twoeye.fincore_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FincoreBackendApplication {
 
 	public static void main(String[] args) {
