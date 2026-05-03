11448e996dbc65b94d205c14a3d93d4c49fbed0d
강성훈 <kshevl123@gmail.com>
2026-05-03 20:07:37 +0900

    chore:Spring Security 생성자 추가

--- src/main/java/com/Twoeye/fincore_backend/config/SecurityConfig.java
+++ src/main/java/com/Twoeye/fincore_backend/config/SecurityConfig.java
@@ -63,8 +63,7 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return new ProviderManager(provider);
     }
